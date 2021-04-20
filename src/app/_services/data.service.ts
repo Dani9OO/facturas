414092f105a06cbc97f66ba04d745d28e1f6f411
dani9oo <dani9oo.dev@gmail.com>
2021-04-20 18:16:08 -0500

    Corregir verificación de existencia Añadir colección de culpa

--- src/app/_services/data.service.ts
+++ src/app/_services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { Factura } from '../common.interfaces';
+import { Factura, Culpa } from '../common.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +9,20 @@ import { Factura } from '../common.interfaces';
 export class DataService {
 
   public facturas$: Observable<Factura[]>;
+  public facturasCollection: AngularFirestoreCollection<Factura>;
 
   constructor(private firestore: AngularFirestore) {
     this.facturas$ = this.firestore.collection<Factura>('facturas').valueChanges();
+    this.facturasCollection = this.firestore.collection<Factura>('facturas');
   }
 
   async newFactura(evt: string): Promise<string | Error> {
-    let existe = false;
-    this.facturas$.subscribe(x => {
-      existe = x.find(factura => factura.remision === evt) ? true : false;
-    });
-    if (existe) return new Error(`Factura ${evt} ya existe`);
     try {
+      const existe = this.firestore.collection<Factura>('facturas', ref => ref.where('remision', '==', evt));
+      if (existe) {
+        await this.firestore.collection<Culpa>('culpa').add({ empleado: 'Raúl' });
+        return new Error(`Factura ${evt} ya existe`);
+      }
       await this.firestore.collection('facturas').add({ remisión: evt });
       return `Factura ${evt} registrada con éxito`
     } catch (error) {
